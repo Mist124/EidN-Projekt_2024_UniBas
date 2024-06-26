7a0784ac6bca91f40e7485662e192f3fa6092a4d
Pascal von Fellenberg <pascal.vonfellenberg@stud.unibas.ch>
2024-06-26 19:56:11 +0200

    chore: make plots of levy and shubert function more presentable (A2_1)

--- A2_1.m
+++ A2_1.m
@@ -1,28 +1,34 @@
 close all; clc; clear;
 
-[X, Y] = meshgrid(-10:0.1:10);
+[X, Y] = meshgrid(-10:0.05:10);
 Z = arrayfun(@(x, y) shubert([x, y]), X, Y);
+
 subplot(2, 2, 1);
-surf(X, Y, Z);
+h = surf(X, Y, Z);
+set(h, 'EdgeAlpha',0.3)
 xlabel("x_1");
 ylabel("x_2");
+zlabel("f(x)");
+title("Shubert surf-Plot");
 
-title("Shubert Oberflächengraphen");
 subplot(2, 2, 2);
 contour(X, Y, Z);
 xlabel("x_1");
 ylabel("x_2");
-title("Shubert Konturgraphen");
+title("Shubert contour-Plot");
 
-[X, Y] = meshgrid(-5.1:0.1:5.1);
+[X, Y] = meshgrid(-5.1:0.05:5.1);
 Z = arrayfun(@(x, y) levy([x, y]), X, Y);
 subplot(2, 2, 3);
-surf(X, Y, Z);
+h = surf(X, Y, Z);
+set(h, 'EdgeAlpha',0.3)
 xlabel("x_1");
 ylabel("x_2");
-title("Levy Oberflächengraphen");
+zlabel("f(x)");
+title("Levy surf-Plot");
+
 subplot(2, 2, 4);
 contour(X, Y, Z);
 xlabel("x_1");
 ylabel("x_2");
-title("Levy Konturgraphen");
+title("Levy contour-Plot");

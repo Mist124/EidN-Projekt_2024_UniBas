7a79f517d94effa0dfc8209af17f48e94815d96b
Pascal von Fellenberg <pascal.vonfellenberg@stud.unibas.ch>
2024-06-26 19:56:58 +0200

    chore: finalize A2_2 script

--- A2_2.m
+++ A2_2.m
@@ -3,29 +3,48 @@ clc; close all; clear;
 params.N = 100;
 params.I_max = 200;
 params.omega = 0.7;
-params.c_k = 0.02;
-params.c_s = 0.02;
+params.c_k = 2;
+params.c_s = 2;
 
-gebiet = [-10 10 -10 10];
+gebiet_l = [-10 10 -10 10];
+gebiet_s = [-5 4 -5 4];
 
-funcH = @(x) levy(x);
+[bestX_l, bestFx_l, xHist_l, bHist_l] = PSO(@levy, gebiet_l, params);
+[bestX_s, bestFx_s, xHist_s, bHist_s] = PSO(@shubert, gebiet_s, params);
 
-[bestX,bestFx,xHist,bHist] = PSO(funcH, gebiet, params);
+[X_l, Y_l] = meshgrid(gebiet_l(1):0.1:gebiet_l(2), gebiet_l(3):0.1:gebiet_l(4));
+Z_l = arrayfun(@(x, y) levy([x, y]), X_l, Y_l);
+[X_s, Y_s] = meshgrid(gebiet_s(1):0.1:gebiet_s(2), gebiet_s(3):0.1:gebiet_s(4));
+Z_s = arrayfun(@(x, y) shubert([x, y]), X_s, Y_s);
 
-[X, Y] = meshgrid(gebiet(1):0.1:gebiet(2), gebiet(3):0.1:gebiet(4));
-Z = arrayfun(@(x, y) funcH([x, y]), X, Y);
-
-for i = 1:9
-    t = int64(i * (params.I_max/9)) + 1;
-    subplot(3, 3, i);
+figure();
+for i = 1:16
+    t = int64(((i-1)*params.I_max)/15)+1;
+    subplot(4, 4, i);
     hold on;
-    contour(X, Y, Z);
-    plot(xHist(:,1,t), xHist(:,2,t), ".");
-    plot(bHist(1,t), bHist(2,t), "*");
+    contour(X_l, Y_l, Z_l);
+    plot(xHist_l(:,1,t), xHist_l(:,2,t), ".");
+    plot(bHist_l(1,t), bHist_l(2,t), "*");
 
-    title("t = " + num2str(t));
+    title("i = " + num2str(t));
     hold off;
 end
 
-%axis([-5.1 5.1 -5.1 5.1]);
-my_utils.renderVideoOfParticlesOnFunction("bruh", funcH, xHist, bHist, gebiet, "levy Funktion PSO");
+figure();
+for i = 1:16
+    t = int64(((i-1)*params.I_max)/15)+1;
+    subplot(4, 4, i);
+    hold on;
+    contour(X_s, Y_s, Z_s);
+    plot(xHist_s(:,1,t), xHist_s(:,2,t), ".");
+    plot(bHist_s(1,t), bHist_s(2,t), "*");
+
+    title("i = " + num2str(t));
+    hold off;
+end
+
+
+
+
+%%
+my_utils.renderVideoOfParticlesOnFunction("PSO", funcH, xHist, bHist, gebiet, "levy Funktion PSO");
